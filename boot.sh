b3725d4ae6a3ee4a86d18a20836f997a6e7772ec
Bui <buicu01@ca.com>
2019-01-28 00:50:56 +0700

    add sleep to boot

--- boot.sh
+++ boot.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-sleep 30
 source venv/bin/activate
 flask db upgrade
 exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app
+sleep 10
+flask db upgrade
+sleep 10
+flask db upgrade
+sleep 10
+flask db upgrade
